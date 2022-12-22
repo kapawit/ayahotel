1ef13627a950036e8133cf591f64e79de0d59aea
kapawit <wahib05jhf@gmail.com>
2022-12-22 18:12:16 +0700

    upload lates db

--- dbhotelaya.sql
+++ dbhotelaya.sql
@@ -47,11 +47,14 @@ CREATE TABLE IF NOT EXISTS `inap` (
   KEY `id_tamu` (`id_tamu`),
   KEY `id_kamar` (`id_kamar`),
   KEY `id_user` (`id_user`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table dbhotelaya.inap: ~0 rows (approximately)
 DELETE FROM `inap`;
 /*!40000 ALTER TABLE `inap` DISABLE KEYS */;
+INSERT INTO `inap` (`id`, `id_tamu`, `id_kamar`, `id_user`, `tgl_awal`, `tgl_akhir`, `tgl_masuk`, `tgl_keluar`, `timestamp`) VALUES
+	(1, 1, 1, 11, '2022-12-15 17:34:41', '2022-12-23 17:34:43', NULL, NULL, '2022-12-22 17:34:48'),
+	(2, 5, 1, 11, '2022-12-20 18:02:50', '2022-12-23 00:00:00', NULL, NULL, '2022-12-22 18:04:31');
 /*!40000 ALTER TABLE `inap` ENABLE KEYS */;
 
 -- Dumping structure for table dbhotelaya.invoice
@@ -67,11 +70,14 @@ CREATE TABLE IF NOT EXISTS `invoice` (
   KEY `id_tamu` (`id_tamu`),
   KEY `id_user` (`id_user`),
   KEY `kode_inv` (`kode_inv`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Dumping data for table dbhotelaya.invoice: ~0 rows (approximately)
+-- Dumping data for table dbhotelaya.invoice: ~2 rows (approximately)
 DELETE FROM `invoice`;
 /*!40000 ALTER TABLE `invoice` DISABLE KEYS */;
+INSERT INTO `invoice` (`id`, `id_tamu`, `id_user`, `kode_inv`, `dp`, `status`, `timestamp`) VALUES
+	(1, 1, 1, 'INV/123', 100000.00, '0', '2022-12-17 17:27:07'),
+	(2, 2, 1, 'INV/124', 10000.00, '0', '2022-12-18 17:28:53');
 /*!40000 ALTER TABLE `invoice` ENABLE KEYS */;
 
 -- Dumping structure for table dbhotelaya.kamar
@@ -82,13 +88,18 @@ CREATE TABLE IF NOT EXISTS `kamar` (
   `status` enum('0','1') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_tipe` (`id_tipe`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8;
 
--- Dumping data for table dbhotelaya.kamar: ~1 rows (approximately)
+-- Dumping data for table dbhotelaya.kamar: ~3 rows (approximately)
 DELETE FROM `kamar`;
 /*!40000 ALTER TABLE `kamar` DISABLE KEYS */;
 INSERT INTO `kamar` (`id`, `id_tipe`, `no_kamar`, `status`) VALUES
-	(1, 1, 'R001', '1');
+	(1, 1, 'R001', '0'),
+	(11, 2, 'R345', '0'),
+	(24, 1, '765', '1'),
+	(25, 2, '123', '0'),
+	(26, 1, '123', '1'),
+	(27, 1, '1234', '1');
 /*!40000 ALTER TABLE `kamar` ENABLE KEYS */;
 
 -- Dumping structure for table dbhotelaya.reservasi
@@ -97,18 +108,21 @@ CREATE TABLE IF NOT EXISTS `reservasi` (
   `id_tamu` int(5) DEFAULT NULL,
   `id_user` int(3) DEFAULT NULL,
   `id_tipe` int(2) DEFAULT NULL,
-  `tgl-awal` datetime DEFAULT NULL,
+  `tgl_awal` datetime DEFAULT NULL,
   `tgl_akhir` datetime DEFAULT NULL,
   `timestamp` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_tamu` (`id_tamu`),
   KEY `id_user` (`id_user`),
   KEY `id_tipe` (`id_tipe`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 -- Dumping data for table dbhotelaya.reservasi: ~0 rows (approximately)
 DELETE FROM `reservasi`;
 /*!40000 ALTER TABLE `reservasi` DISABLE KEYS */;
+INSERT INTO `reservasi` (`id`, `id_tamu`, `id_user`, `id_tipe`, `tgl_awal`, `tgl_akhir`, `timestamp`) VALUES
+	(1, 1, 1, 2, '2022-12-01 16:15:53', '2022-12-23 00:00:00', '2022-12-22 16:16:04'),
+	(2, 11, 1, 1, '2022-12-20 18:02:50', '2022-12-23 00:00:00', '2022-12-22 18:04:03');
 /*!40000 ALTER TABLE `reservasi` ENABLE KEYS */;
 
 -- Dumping structure for table dbhotelaya.tamu
@@ -117,30 +131,42 @@ CREATE TABLE IF NOT EXISTS `tamu` (
   `nama` varchar(50) DEFAULT NULL,
   `no_ktp` char(16) DEFAULT NULL,
   `no_telp` char(13) DEFAULT NULL,
-  `status_member` enum('Y','N') DEFAULT NULL,
-  `timestamp` timestamp NULL DEFAULT NULL,
+  `status_member` enum('0','1') DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `no_telp` (`no_telp`),
   KEY `no_ktp` (`no_ktp`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
 
--- Dumping data for table dbhotelaya.tamu: ~0 rows (approximately)
+-- Dumping data for table dbhotelaya.tamu: ~1 rows (approximately)
 DELETE FROM `tamu`;
 /*!40000 ALTER TABLE `tamu` DISABLE KEYS */;
+INSERT INTO `tamu` (`id`, `nama`, `no_ktp`, `no_telp`, `status_member`) VALUES
+	(1, 'pawit wahib', '7897879', '98798798', '0'),
+	(4, 'ajskbdkjb', '319826398', '176876', '0'),
+	(5, 'tes', '96889987', '987897', '0'),
+	(6, 'kjgkj', '1234', '1234', '0'),
+	(7, 'jhgjhg', '7786786', '87678', '0'),
+	(8, 'lkdklnalk', '876876', '876876', '0'),
+	(9, 'kajsbdkj', '876', '887678', '0'),
+	(11, 'aksbdjka', '6876', '8587875', '0'),
+	(13, 'jhvsadjvh', '798798', '87989798', '0');
 /*!40000 ALTER TABLE `tamu` ENABLE KEYS */;
 
 -- Dumping structure for table dbhotelaya.tipe_kamar
 CREATE TABLE IF NOT EXISTS `tipe_kamar` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `kategori` varchar(10) DEFAULT NULL,
-  `harga` decimal(10,2) DEFAULT NULL,
-  `total_kamar` int(4) DEFAULT NULL,
+  `kategori` varchar(10) NOT NULL,
+  `harga` decimal(10,2) NOT NULL,
+  `total_kamar` int(4) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
--- Dumping data for table dbhotelaya.tipe_kamar: ~0 rows (approximately)
+-- Dumping data for table dbhotelaya.tipe_kamar: ~2 rows (approximately)
 DELETE FROM `tipe_kamar`;
 /*!40000 ALTER TABLE `tipe_kamar` DISABLE KEYS */;
+INSERT INTO `tipe_kamar` (`id`, `kategori`, `harga`, `total_kamar`) VALUES
+	(1, 'VIP', 500000.00, 20),
+	(2, 'REGULAR', 200000.00, 100);
 /*!40000 ALTER TABLE `tipe_kamar` ENABLE KEYS */;
 
 -- Dumping structure for table dbhotelaya.user
@@ -153,7 +179,7 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
--- Dumping data for table dbhotelaya.user: ~1 rows (approximately)
+-- Dumping data for table dbhotelaya.user: ~0 rows (approximately)
 DELETE FROM `user`;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 INSERT INTO `user` (`id`, `nama`, `username`, `password`, `level`) VALUES
